8008b5f57b60b25c561f9c44fc37819a3e0889fb
focus9345 <connor.joshua@gmail.com>
2020-10-13 14:10:51 -0400

    build changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="jumbotron">
+        <h1 className="display-4">Hello, world!</h1>
+        <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+      </div>
+      <div className="conatiner">
+        <div className="row">
+          <div className="col-md">Sample One</div>
+          <div className="col-md">Sample Two</div>
+          <div className="col-md">Sample Three</div>
+        </div>
+      </div>
     </div>
   );
 }
-
 export default App;

6489c481fddacfa8d62c1c5a6e5f05145dcb0fd3
focus9345 <connor.joshua@gmail.com>
2020-10-15 08:29:56 -0400

    update show 2 day results

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -5,22 +5,17 @@ export interface Props {
 }
 
 const Item: React.FC<Props> = ({ data }) => {
-    const lastFiveDays : number = 5;
+    const lastFiveDays : number = 2;
     const smallArray = data.slice(0, lastFiveDays);
     return (
         <div className="row"> 
             { smallArray.map((item: any, index: number) => (
-                
-                <div className="col-md col" key={index}>
+                <div className="col" key={index}>
                 <div className="card" >
                     <div className="card-body" >
-                        <h5 className="card-title text-secondary" > {
-                            item.date.toString().substring(0, 4) 
-                            + "-"
-                            + item.date.toString().substring(4, 2) 
-                            + "-"
-                            + item.date.toString().slice(6, 2)
-                            } </h5>
+                        <h5 className="card-title text-secondary" > 
+                            { item.date } 
+                         </h5>
                     </div>
                     <ul className="list-group list-group-flush" >
                         { Object.entries(item).map(([keyCov, valueCov]) => (

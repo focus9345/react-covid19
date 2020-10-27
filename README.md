540e96e997a1f637f778e5118ed2f03f2f8af2b6
focus9345 <connor.joshua@gmail.com>
2020-10-27 13:53:26 -0400

    update to read me file

--- README.md
+++ README.md
@@ -1,3 +1,31 @@
+# Covid 19 Stats React App
+
+This web application intends to display changing report statistics on the covid 19 statistics. The app takes API reports from [covidtracking](https://api.covidtracking.com/v1/us/daily.json) and uses the d3.js library to display the statistics.
+
+**The application uses the following technology:**
+* React 16.13
+* Typescript 3.7
+* d3.js 6.3
+* Node.js 13.12
+* Bootstrap 4.5
+* node-sass 4.14
+
+The app is still in the **development phase** and has about _65%_ completed. 
+
+## Tasks
+
+- [x] Setup Fetch to API Json using typescript
+- [x] Display seven days worth of results
+- [x] Setup a D3 Bargraph to display the last seven days worth of positive cases in the United Sttates
+- [] Setup a D3 pie graph of yesterdays negative, positive, us populattion
+- [] Set up a D3 line graph of new positive cases over the last seven days
+- [] Review Page Design for responsiveness, browser test
+- [] Test if data fails to load, display something to end users
+
+
+
+To install node to run this application locally, please visit: [Node.js](https://nodejs.org/en/)
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
@@ -8,37 +36,3 @@ In the project directory, you can run:
 
 Runs the app in the development mode.<br />
 Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
-
-The page will reload if you make edits.<br />
-You will also see any lint errors in the console.
-
-### `npm test`
-
-Launches the test runner in the interactive watch mode.<br />
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.<br />
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.<br />
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
--- src/components/Charts.tsx
+++ src/components/Charts.tsx
@@ -1,52 +1,131 @@
 import React, { useRef, useState, useEffect } from 'react';
 import { select, Selection } from 'd3-selection';
-import { time } from 'console';
+import { scaleLinear, scaleBand } from 'd3-scale';
+import { max } from 'd3-array';
+import { axisLeft, axisBottom } from 'd3-axis';
+import { DEFAULT_MIN_VERSION } from 'tls';
 
 
 interface Covid {
     data: any;
 }
 
+// Chart Dimensions
+const dimensions = {
+    width: 800,
+    height: 500,
+    chartWidth: 700,
+    chartHeight: 380,
+    marginLeft: 100,
+    marginTop: 20
+}
+
+function formatDate(date:  Date) {
+    var d = new Date(date),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+
+    if (month.length < 2) 
+        month = '0' + month;
+    if (day.length < 2) 
+        day = '0' + day;
+
+    return [year, month, day].join('-');
+}
+
 function dataDatePos(covidData: any) {
-    const arrayDatePos = covidData.map( (item: any) => ({
-        pos: item.positive,
-        date: item.dateChecked
+    const arrayDatePos: { name: string; number: number}[] = covidData.map( (item: any) => ({
+        name: formatDate(item.dateChecked),
+        number: item.positive
     }));
-    console.log(JSON.stringify(arrayDatePos));
     return arrayDatePos;
 }
 
 function displayData(covidStats: any) {
     // How many days should we compair
     const smallArray = covidStats.slice(0, 7);
-    
-    dataDatePos(smallArray);
-    return smallArray;
+    // We just want dates and positive cases
+    return dataDatePos(smallArray);
 }
 
 const Item: React.FC<Covid> = ({ data }) => {
     
     const refinedData = displayData(data);
-    //console.log('List: ', refinedData);
+    console.log('List: ', JSON.stringify(refinedData));
+
 
     const ref = useRef<SVGSVGElement | null>(null);
-    const [selection, setSelection] = useState<null | Selection<
-        SVGSVGElement | null, 
-        unknown, 
-        null, 
-        undefined
-    >>(null);
+    const [selection, setSelection] = useState< null | Selection<SVGSVGElement | null, unknown, null, undefined>>(null);
+
+    
+
+    const maxValue = max(refinedData, d => {
+        const xValue = d.number;
+        if(xValue) {
+            return xValue;
+        }
+        return 100;
+    });
+    const yScale = scaleLinear()
+        .domain([0, maxValue!])
+        .range([dimensions.chartHeight, 0]);
+
+    const xScale = scaleBand()
+        .domain(refinedData.map(d  => d.name))
+        .range([0, dimensions.width])
+        .paddingInner(0.4);
+
+    const yAxis = axisLeft(yScale).ticks(5);
+    const xAxis = axisBottom(xScale);
     
     useEffect(() => {
         if(!selection){
             setSelection(select(ref.current))
-        } 
+        } else {
+
+            // selection
+            //     .append('rect')
+            //     .attr('width', dimensions.width)
+            //     .attr('height', dimensions.height)
+            //     .attr('fill', 'blue')
+            const xAxisGroup = selection
+                .append('g')
+                .attr('transform', `translate(${dimensions.marginLeft}, ${dimensions.chartHeight})`)
+                .call(xAxis)
+
+            const yAxisGroup = selection
+                .append('g')
+                .attr('transform', `translate(${dimensions.marginLeft}, 0)`)
+                .call(yAxis)
+
+            selection
+                .append('g')
+                .attr('transform', `translate(${dimensions.marginLeft}, 0)`)
+                .selectAll('rect')
+                .data(refinedData)
+                .enter()
+                .append('rect')
+                .attr('width', xScale.bandwidth)
+                .attr('height', d => dimensions.chartHeight - (yScale(d.number) || 0) )
+                .attr('x', d => {
+                    const xValue = xScale(d.name);
+                    if(xValue) {
+                        return xValue;
+                    }
+                    return null;
+                })
+                .attr('y', d => yScale(d.number) || 0)
+                .attr('fill', '#3898EB');
+                
+        }
     }, [selection])
     return (
         <div className="row justify-content-center"> 
             <div className="col">
-                <h2 className="text-primary">US Positive Covid Cases</h2>
-                <svg ref={ref} width={800} height={400} />
+                <h2 className="text-primarytext-center text-center">Total of US Positive Covid-19 Cases</h2>
+                <p className="text-secondary text-center">The bar chart shows the number of covid cases in the United States by day.</p>
+                <svg ref={ref} width={dimensions.width} height={dimensions.height} className='center-graph' />
             </div>
         </div>
     );
